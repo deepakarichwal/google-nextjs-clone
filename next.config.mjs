ae519625a879bf0c4c8a91dc0c5223e4cee555d3
deepakarichwal <deepakarichwal03@gmail.com>
2024-02-15 01:09:52 +0530

    Removed the experimental option from next config file

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  // experimental: {
+  //   missingSuspenseWithCSRBailout: false,
+  // },
 };
 
 export default nextConfig;
